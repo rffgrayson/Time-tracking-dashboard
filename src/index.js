ba76ea3499d947d87770e020ee61288e61787009
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-11-22 12:22:28 +0800

    add logic to update dashboard and test it with daily button first

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import data from './data.json';
 import "./styles.css";
 
 console.log(data);
+
+function updateDashboard (timeframe) {
+    data.forEach(item => {
+        const id = item.title.toLowerCase().replace(' ', '-');
+        const {current, previous} = item.timeframes[timeframe];
+
+        document.getElementById(`${id}-current-hours`).textContent = `${current}hrs`;
+        document.getElementById(`${id}-previous-hours`).textContent = `Previous - ${previous}hrs`;
+    });
+}
+
+document.getElementById('daily-btn').addEventListener('click', () => {
+    updateDashboard('daily');
+  });
